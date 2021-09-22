e2850f79c4b84782503c3b37600b43cb68de9951
Chris <chris.feight@gmail.com>
2021-09-22 16:52:54 -0700

    test commit of after-hours RPS

---
+++ week-02/03-JavaScript/01-Activities/28-Stu_Mini-Project/TODO.js
@@ -0,0 +1,55 @@
+//TODO: prompt user R, P,S save that to a variable
+//edge case: what if user doesn't pick one of those?
+//edge case: what if they use lowercase letters?
+
+
+//WHEN I input R, P, or S
+//THEN my output returns the same R, P, or S
+//WHEN 
+
+//TODO: create a simple layout on HTML that says "thanks for playing"
+//TODO: create an array with possible outcomes
+//TODO: Prompt R,P, or S
+//TODO: Check if input was R/P/S, else throw an early return with error
+//TODO: save the input to an empty variable
+//TODO: select a random number to correspond to the RPS array
+//TODO: save the randomly selected option to another empty variable
+//TODO: create if/else formula based on the results
+//TODO: generate a popup with results
+//TODO: create an empty variable for user score, computer score, tie 
+//TODO: ^ needs to display at the end of each round
+//TODO: prompt user after each round to play again
+
+
+//TODO: make 4 arrays for lowercase, uppercase, numbers, characters
+
+// const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+// const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y" "z"];
+
+// const upperCase = "[A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// const spclChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", ".", ","];
+
+//TODO: generate computer choice
+//three outcomes; r, p, or s
+//randomly choose one
+//save as a variable
+
+//TODO: compare choices
+    //win
+        //user rock, comp scissors || user paper comp rock || user scissors comp paper
+    //loss
+        //comp rock, user scissors || comp paper user rock || comp scissors user paper
+    //tie
+        //comp rock user rock || comp paper user paper || comp scissors user scissors
+
+//TODO: show result
+    //with a popup
+
+//TODO: show cumlative score
+    //include wins losses and ties
+    //need to track across games
+
+//TODO: ask to play again
+    //start over from top
--- week-02/04-Web-APIs/01-Activities/06-Stu_Setting-Attributes/Unsolved/assets/js/script.js
+++ week-02/04-Web-APIs/01-Activities/06-Stu_Setting-Attributes/Unsolved/assets/js/script.js
@@ -1,6 +1,7 @@
 var site1El = document.querySelector(".site1");
 var site2El = document.querySelector(".site2");
 var site3El = document.querySelector(".site3");
+var h4s = document.querySelectorAll("h4")
 
 site1El.children[0].textContent = "Site 1";
 site1El.children[1].setAttribute("href", "https://google.com");
@@ -21,3 +22,14 @@ site3El.children[1].children[0].setAttribute("alt", "women working");
 site3El.children[1].children[0].setAttribute("style", "padding:10px;");
 
 // Write Your Code Below
+
+console.log(h4s)
+
+for (let i = 0; i < h4s, length; i++) {
+    h4s[i].setAttribute("style", "font-size:30px; color:blue; font-weight:bold; margin:0; padding-left:10px")
+}
+
+//var h4Elements = document.querySelectorAll("h4")
+//h4Elements.children[0].setAttribute = "h4";
+//h4Elements.children[1].setAttribute("h4");
+//h4Elements.children[1].children[0].setAttribute("style", "font-size: 300px");
